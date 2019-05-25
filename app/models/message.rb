30029032db3e121dd67f36b63928f3fc1aa3fbaa
AtoraYamada <atora2018@gmail.com>
2019-05-25 09:18:22 +0900

    rename messages column image to picture

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   
-  validates :body, presence: true, unless: :image?
-  mount_uploader :image, ImageUploader
+  validates :body, presence: true, unless: :picture?
+  mount_uploader :picture, ImageUploader
 end
