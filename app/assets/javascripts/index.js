4eb76bca9797c930c12ff5a654614da144e7f91f
AtoraYamada <atora2018@gmail.com>
2019-05-29 19:18:35 +0900

    fix search result when input is null

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,11 +1,19 @@
 $(function() {
   var serach_result = $('#user-search-result');
-  function appendUser(user){
+  function appendUserToResult(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
   serach_result.append(html);
+  }
+  function appendUserToGroup(user){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${user.id}'>
+                  <input name='group[user_ids][]' type='hidden' value='${user.id}>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+  $('#chat-group-users').append(html);
   }
   function appendErrMsgToHTML(msg) {
   var html = `<div class="chat-group-user clearfix">
@@ -16,27 +24,30 @@ $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $(this).val();
     console.log(input)
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-    .done(function(users) {
-      console.log(users)
-      serach_result.empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      }
-      else {
-        appendErrMsgToHTML("一致するユーザーが見つかりません");
-      }
-    })
-    .fail(function() {
-      alert('ユーザー検索に失敗しました');
-    })
+    serach_result.empty();
+    if (input.length !== 0){
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+      .done(function(users) {
+        console.log(users)
+        serach_result.empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
+            appendUserToResult(user);
+          });
+        }
+        else {
+          appendErrMsgToHTML("一致するユーザーが見つかりません");
+        }
+      })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました');
+      })
+    }
   });
   $('#user-search-result').on('click', '.user-search-add', function(){
     $(this).parent().remove();
