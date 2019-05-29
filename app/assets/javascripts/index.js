935eb908b0095dd831041c5f91825397feee1ce8
AtoraYamada <atora2018@gmail.com>
2019-05-29 18:16:21 +0900

    def .done and .fail

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,4 +1,15 @@
 $(function() {
+  function appendUser(user){
+    var htnl = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+  }
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${msg}</p>
+                </div>`
+  }
   $("#user-search-field").on("keyup", function() {
     var input = $(this).val();
     console.log(input)
@@ -10,6 +21,15 @@ $(function() {
     })
     .done(function(users) {
       console.log(users)
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーが見つかりません");
+      }
+      var html = buildHTML(users)
     })
     .fail(function() {
       alert('ユーザー検索に失敗しました');
