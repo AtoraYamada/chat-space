0daca549cb3b9e2e7d78b94a6412acd46d8a5f9c
AtoraYamada <atora2018@gmail.com>
2019-05-29 18:22:25 +0900

    reflect search result to form

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,14 +1,17 @@
 $(function() {
+  var serach_result = $('#user-search-result');
   function appendUser(user){
-    var htnl = `<div class="chat-group-user clearfix">
+    var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
+  serach_result.append(html);
   }
   function appendErrMsgToHTML(msg) {
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${msg}</p>
-                </div>`
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${msg}</p>
+              </div>`
+  serach_result.append(html);
   }
   $("#user-search-field").on("keyup", function() {
     var input = $(this).val();
@@ -21,7 +24,7 @@ $(function() {
     })
     .done(function(users) {
       console.log(users)
-      $('#user-search-result').empty();
+      serach_result.empty();
       if (users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
@@ -30,7 +33,6 @@ $(function() {
       else {
         appendErrMsgToHTML("一致するユーザーが見つかりません");
       }
-      var html = buildHTML(users)
     })
     .fail(function() {
       alert('ユーザー検索に失敗しました');
