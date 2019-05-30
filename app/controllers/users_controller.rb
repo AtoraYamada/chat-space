75b1252737e0ce48fc90b8c1698055f4b486a1d3
AtoraYamada <atora2018@gmail.com>
2019-05-30 13:34:14 +0900

    exclude current_user in users#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(75)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where('id != ?', current_user.id).limit(75)
     respond_to do |format|
       format.html
       format.json
