135043ac5881201aa6ac9330c1f23f56e3626caa
AtoraYamada <atora2018@gmail.com>
2019-05-31 21:15:32 +0900

    add divided case for if last_message_id is undefined

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,9 @@
 class Api::MessagesController < ApplicationController
   def index
-    @new_messages=Message.where('id > ? AND group_id = ?', params[:id], params[:group_id])
+    if params[:id].nil?
+      @new_messages=Message.where('group_id = ?', params[:group_id])
+    else
+      @new_messages=Message.where('id > ? AND group_id = ?', params[:id], params[:group_id])
+    end
   end
 end
